91863ec55534fe633d330bea0afb45c025ba344b
Neil Mallinar <nmallinar@gmail.com>
2024-02-02 16:35:25 -0800

    check in rfm

--- grokking/rfm.py
+++ grokking/rfm.py
@@ -3,21 +3,40 @@ import torch
 from numpy.linalg import solve
 import classic_kernel
 from tqdm import tqdm
+import math
 from torch import nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 #import hickle
+import scipy
+from inf_ntk import ntk_fn, jax_ntk_fn
+from sklearn.metrics import top_k_accuracy_score
 
 def main(num_tokens, dim_model, train_loader, test_loader, wandb, L, embedding_layer, agop_weight):
     rfm(train_loader, test_loader, embedding_layer, num_tokens, wandb,
-            iters=1, name=None, batch_size=2, reg=1e-5,
+            iters=10, name=None, batch_size=2, reg=1e-3,
             train_acc=True, L=L, agop_weight=agop_weight)
+
+def ntk_M(pair1, pair2, depth, M):
+    _, ntk = ntk_fn(pair1, pair2, M=M, depth=int(depth), bias=0, jax_rescale=True)
+    return ntk
+
+def jax_ntk_M(pair1, pair2, depth, M):
+    _, ntk = jax_ntk_fn(pair1, pair2, M, depth=int(depth))
+    return ntk
 
 def laplace_kernel_M(pair1, pair2, bandwidth, M):
     return classic_kernel.laplacian_M(pair1, pair2, bandwidth, M)
 
 def gaussian_kernel_M(pair1, pair2, bandwidth, M):
     return classic_kernel.gaussian_M(pair1, pair2, bandwidth, M)
+
+def kernel_fn(pair1, pair2, bandwidth, M):
+    # overloading bandwidth param to be depth in NTK
+    # return ntk_M(pair1, pair2, bandwidth, M)
+    # return jax_ntk_M(pair1, pair2, bandwidth, M)
+    return laplace_kernel_M(pair1, pair2, bandwidth, M)
+    #return gaussian_kernel_M(pair1, pair2, bandwidth, M)
 
 def get_grads(X, sol, L, P, batch_size=2):
     M = 0.
@@ -30,7 +49,7 @@ def get_grads(X, sol, L, P, batch_size=2):
     else:
         x = X
 
-    K = laplace_kernel_M(X, x, L, P)
+    K = kernel_fn(X, x, L, P)
 
     dist = classic_kernel.euclidean_distances_M(X, x, P, squared=False)
     dist = torch.where(dist < 1e-10, torch.zeros(1).float(), dist)
@@ -69,7 +88,8 @@ def get_grads(X, sol, L, P, batch_size=2):
 
     bs = batch_size
     batches = torch.split(G, bs)
-    for i in tqdm(range(len(batches))):
+    for i in range(len(batches)):
+    # for i in tqdm(range(len(batches))):
         # grad = batches[i].cuda()
         grad = batches[i]
         gradT = torch.transpose(grad, 1, 2)
@@ -77,7 +97,7 @@ def get_grads(X, sol, L, P, batch_size=2):
         del grad, gradT
     torch.cuda.empty_cache()
     M /= len(G)
-    M = M.numpy()
+    M = np.real(scipy.linalg.sqrtm(M.numpy()))
 
     return M
 
@@ -91,7 +111,7 @@ def get_grad_reg(X, L, P):
     else:
         x = X
 
-    K = laplace_kernel_M(X, x, L, P)
+    K = kernel_fn(X, x, L, P)
 
     dist = classic_kernel.euclidean_distances_M(X, x, P, squared=False)
     dist = torch.where(dist < 1e-10, torch.zeros(1).float(), dist)
@@ -116,28 +136,53 @@ def rfm(train_loader, test_loader, embedding_layer, num_classes, wandb,
         iters=3, name=None, batch_size=2, reg=1e-3,
         train_acc=False, L=1, agop_weight=1e-5):
 
-    X_train, y_train, true_y_train = get_data(train_loader, embedding_layer, num_classes)
-    X_test, y_test, true_y_test = get_data(test_loader, embedding_layer, num_classes)
+    # X_train, y_train, true_y_train = get_data(train_loader, embedding_layer, num_classes)
+    # X_test, y_test, true_y_test = get_data(test_loader, embedding_layer, num_classes)
+
+    # X_train = torch.tensor(np.load('data/10k/base_train_data.npy')).double()
+    # true_y_train = torch.tensor(np.load('data/10k/base_train_labels.npy'))
+    # y_train = F.one_hot(true_y_train, num_classes).double().double()
+
+    X_test = torch.tensor(np.load('data/100k/base_val_feats.npy')).double()
+    true_y_test = torch.tensor(np.load('data/100k/base_val_labels.npy'))
+    y_test = F.one_hot(true_y_test, num_classes).double()
+
+    X_train = torch.tensor(np.load('data/100k/synthetic_data.npy')).double()
+    y_train = torch.tensor(np.load('data/100k/synthetic_labels.npy')).double()
+    true_y_train = torch.tensor(np.argmax(y_train, axis=-1))
 
     n, d = X_train.shape
 
     M = np.eye(d, dtype='float64')
+    # M = np.load('grokking_outputs/nov27_proper_agop/ep_1000_neural_feature_matrix.npy').astype(np.float64)
+    # l, v = np.linalg.eigh(M)
+    # idx = np.argsort(l)[::-1]
+    # l = l[idx]
+    # v = v[idx]
+    # # M = v @ np.diag(l / 256) @ v.T
+    # M = v[:,:128] @ np.diag(l[:128]) @ v[:,:128].T
+
+    # M = scipy.linalg.sqrtm(M)
+    # M = np.maximum(M, 0)
+    # M = M.T @ M
     for i in range(iters):
-        G_reg = get_grad_reg(X_train, L, torch.from_numpy(M)).numpy()
-        K_train = laplace_kernel_M(X_train, X_train, L, torch.from_numpy(M)).numpy()
-        sol = np.linalg.inv(K_train.T @ K_train + agop_weight*G_reg) @ K_train @ y_train.numpy()
-        sol = sol.T
-        # sol = solve(K_train + reg * np.eye(len(K_train)), y_train).T
+        # G_reg = get_grad_reg(X_train, L, torch.from_numpy(M)).numpy()
+        K_train = kernel_fn(X_train, X_train, L, torch.from_numpy(M)).numpy()
+        # sol = np.linalg.inv(K_train.T @ K_train + agop_weight*G_reg) @ K_train @ y_train.numpy()
+        # sol = sol.T
+        sol = solve(K_train + reg * np.eye(len(K_train)), y_train).T
 
         if train_acc:
             preds = (sol @ K_train).T
             loss = np.mean(np.square(preds - y_train.numpy()))
-            print("Round " + str(i) + " Train MSE: ", loss) # Loss function
             y_pred = torch.from_numpy(preds)
             preds = torch.argmax(y_pred, dim=-1)
             labels = torch.argmax(y_train, dim=-1)
             count = torch.sum(true_y_train == preds).numpy()
             acc = count / len(labels)
+
+            #if i % 50 == 0:
+            print("Round " + str(i) + " Train MSE: ", loss) # Loss function
             print("Round " + str(i) + " Train Acc: ", acc)
 
             metrics = {
@@ -147,16 +192,22 @@ def rfm(train_loader, test_loader, embedding_layer, num_classes, wandb,
             }
             wandb.log(metrics)
 
-        K_test = laplace_kernel_M(X_train, X_test, L, torch.from_numpy(M)).numpy()
+        # if i % 50 == 0:
+        top_k_val = 5
+        K_test = kernel_fn(X_train, X_test, L, torch.from_numpy(M)).numpy()
         preds = (sol @ K_test).T
         loss = np.mean(np.square(preds - y_test.numpy()))
         print("Round " + str(i) + " MSE: ", loss) # Loss function
         y_pred = torch.from_numpy(preds)
         preds = torch.argmax(y_pred, dim=-1)
         labels = torch.argmax(y_test, dim=-1)
+        top_k_acc = top_k_accuracy_score(labels, y_pred, k=top_k_val)
+
         count = torch.sum(true_y_test == preds).numpy()
         acc = count / len(labels)
         print("Round " + str(i) + " Acc: ", acc)
+        print("Round " + str(i) + f" Top {top_k_val} Acc: ", top_k_acc)
+        print()
 
         metrics = {
             'validation/accuracy': acc,
@@ -167,15 +218,15 @@ def rfm(train_loader, test_loader, embedding_layer, num_classes, wandb,
 
 
         M  = get_grads(X_train, sol, L, torch.from_numpy(M), batch_size=batch_size)
-        plt.imshow(M)
-        plt.savefig(f'M_unique_round{i}.pdf')
+        # plt.imshow(M)
+        # plt.savefig(f'M_unique_round{i}.pdf')
 
         # if name is not None:
         #     hickle.dump(M, 'saved_Ms/M_' + name + '_' + str(i) + '.h')
 
-    K_train = laplace_kernel_M(X_train, X_train, L, torch.from_numpy(M)).numpy()
+    K_train = kernel_fn(X_train, X_train, L, torch.from_numpy(M)).numpy()
     sol = solve(K_train + reg * np.eye(len(K_train)), y_train).T
-    K_test = laplace_kernel_M(X_train, X_test, L, torch.from_numpy(M)).numpy()
+    K_test = kernel_fn(X_train, X_test, L, torch.from_numpy(M)).numpy()
     preds = (sol @ K_test).T
     mse = np.mean(np.square(preds - y_test.numpy()))
     print("Final MSE: ", mse)
@@ -194,7 +245,7 @@ def get_data(loader, embedding_layer, num_classes):
     for idx, batch in enumerate(loader):
         inputs, labels = batch
         batch_size = inputs.shape[0]
-        inputs = torch.stack((inputs[:,0], inputs[:,2]), dim=1)
+        # inputs = torch.stack((inputs[:,0], inputs[:,2]), dim=1)
         X.append(embedding_layer(inputs).view(batch_size, -1))
 
         true_y.append(labels)
---
+++ grokking/submit_cpu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH -J nmallina-grokking          # Job name
+#SBATCH -o logs/nmallina-grokking.%j.log   # define stdout filename; %j expands to jobid; to redirect stderr elsewhere, duplicate this line with -e instead
+#
+#SBATCH --mail-user=nmallina@ucsd.edu
+#SBATCH --mail-type=FAIL,TIME_LIMIT # get notified via email on job failure or time limit reached
+#
+#SBATCH --account bbjr-delta-gpu
+#SBATCH --partition gpuA100x4         # specify queue, if this doesnt submit try gpu-shared
+#SBATCH --gpus-per-node 1
+#SBATCH --cpus-per-task 4
+#SBATCH --mem 8G
+#SBATCH --nodes 1
+#SBATCH --tasks 1
+#SBATCH --tasks-per-node 1
+#SBATCH -t 4:00:00       # set maximum run time in H:M:S
+#SBATCH --no-requeue     # dont automatically requeue job id node fails, usually errors need to be inspected and debugged
+
+python cli.py \
+  --training_fraction 0.5 \
+  --prime 31 \
+  --batch_size 32 \
+  --device cpu \
+  --model "rfm" \
+  --eval_entk -1 \
+  --num_layers 1 \
+  --weight_decay 1.0 \
+  --agop_weight 1.0 \
+  --wandb_offline \
+  --wandb_proj_name "feb2-grokking" \
+  --agop_subsample_n 32 \
+  --learning_rate 1e-3 \
+  --optimizer "adamw" \
+  --momentum 0.0 \
+  --num_steps 100000 \
+  --out_dir "./" \
+  --kernel_bandwidth 1
