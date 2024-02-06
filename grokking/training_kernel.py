d580ea51d85e16b5846ae5bb003643bc1fdc57d4
Neil Mallinar <nmallinar@gmail.com>
2024-02-06 11:26:50 -0600

    update training kernel

--- grokking/training_kernel.py
+++ grokking/training_kernel.py
@@ -2,14 +2,15 @@ import numpy as np
 import scipy.linalg
 import torch
 import os
+import torch.nn.functional as F
 
 from classic_kernel import euclidean_distances, laplacian, gaussian
 from inf_ntk import ep3_ntk_relu
 
 DATA_DIR = '/scratch/bbjr/mallina1/grokking_output/feb2-grokking/tmwu613g/epoch_460'
 n_classes = 31
-n_centers = 40000
-n_train = 1000000
+n_centers = 50000
+n_train = 100000
 ridge = 0.0
 
 # kernel_fn = lambda x, z: ep3_ntk_relu(x, z, depth=10)
@@ -25,7 +26,7 @@ y_train_idx = y_train.argmax(-1)
 
 X_test = torch.tensor(np.load(os.path.join(DATA_DIR, 'base_val_feats.npy'))).float()
 y_test_idx = torch.tensor(np.load(os.path.join(DATA_DIR, 'base_val_labels.npy')))
-y_test = one_hot(y_test_idx, n_classes).float()
+y_test = F.one_hot(y_test_idx, n_classes).float()
 
 right_nfm = torch.tensor(np.load(os.path.join(DATA_DIR, 'right_nfm.npy'))).float()
 right_agop = torch.tensor(np.load(os.path.join(DATA_DIR, 'right_agop_0.npy'))).float()
@@ -42,17 +43,18 @@ K_zx = kernel_fn(centers, X_train)
 
 K_zz = K_zx @ K_zx.T
 
-alpha = torch.linalg.pinv(K_zz + ridge * torch.eye(n_train), hermitian=True) @ K_zx @ y_train
-train_preds = K_xx @ alpha
+alpha = torch.linalg.pinv(K_zz + ridge * torch.eye(n_centers), hermitian=True) @ K_zx @ y_train
+#alpha = torch.linalg.pinv(K_zx) @ y_train
+train_preds = K_zx.T @ alpha
 train_preds = train_preds.argmax(-1)
-accuracy = (train_preds == y_train_idx).mean()
+accuracy = sum(train_preds == y_train_idx) / n_train
 print(f'Training accuracy: {accuracy}')
 
-del K_xx, K_zx
+del K_zz, K_zx
 
 # (m, n)
 K_te = kernel_fn(X_test, centers)
 test_preds = K_te @ alpha
 test_preds = test_preds.argmax(-1)
-accuracy = (test_preds == y_test_idx).mean()
+accuracy = sum(test_preds == y_test_idx) / len(test_preds)
 print(f'Testing accuracy: {accuracy}')
