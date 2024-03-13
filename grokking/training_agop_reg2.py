59efcff7c74504b4cf58dc70258972674e5d8755
Neil Mallinar <nmallinar@gmail.com>
2024-03-13 13:51:17 -0700

    reg full agop

--- grokking/training_agop_reg2.py
+++ grokking/training_agop_reg2.py
@@ -493,14 +493,15 @@ def train(model, train_loader, agop_loader, optimizer, scheduler,
 
 
         if not config.skip_agop_comps:
-            dumb1 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
-            dumb2 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
-            dumb3 = torch.zeros((config.agop_subsample_n, n_classes)).to(config.device)
-
-            dumb4 = torch.zeros((config.agop_subsample_n, model.inp_dim)).to(config.device)
-            dumb5 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
-            dumb6 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
-            final_agops, final_left_agops = calc_batch_agops(model, inputs, dumb1, dumb2, dumb3, dumb4, dumb5, dumb6, config.device, config)
+            final_agops, final_left_agops = calc_full_agops(model, agop_loader, config, num_tokens, embedding_layer=None)
+            # dumb1 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
+            # dumb2 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
+            # dumb3 = torch.zeros((config.agop_subsample_n, n_classes)).to(config.device)
+            #
+            # dumb4 = torch.zeros((config.agop_subsample_n, model.inp_dim)).to(config.device)
+            # dumb5 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
+            # dumb6 = torch.zeros((config.agop_subsample_n, model.hidden_width)).to(config.device)
+            # final_agops, final_left_agops = calc_batch_agops(model, inputs, dumb1, dumb2, dumb3, dumb4, dumb5, dumb6, config.device, config)
 
         # Zero gradient buffers
         optimizer.zero_grad()
