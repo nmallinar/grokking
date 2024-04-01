ade2c8e8db71c172f88634e58871e1cd6bbd601c
Neil Mallinar <nmallinar@gmail.com>
2024-04-01 16:34:19 -0500

    update submit script

--- v2_grokking/submit.sh
+++ v2_grokking/submit.sh
@@ -8,17 +8,17 @@
 #SBATCH --account bbjr-delta-gpu
 #SBATCH --partition gpuA40x4         # specify queue, if this doesnt submit try gpu-shared
 #SBATCH --gpus-per-node 1
-#SBATCH --cpus-per-task 16
+#SBATCH --cpus-per-task 32
 #SBATCH --mem 32G
 #SBATCH --nodes 1
 #SBATCH --tasks 1
 #SBATCH --tasks-per-node 1
-#SBATCH -t 2:00:00       # set maximum run time in H:M:S
+#SBATCH -t 1:00:00       # set maximum run time in H:M:S
 #SBATCH --no-requeue     # dont automatically requeue job id node fails, usually errors need to be inspected and debugged
 
 source /projects/bbjr/mallina1/envs/torch_and_jax/bin/activate
 
-for i in $(seq 1 5);
+for i in $(seq 1 1);
 do
   python train_kernel.py \
     --wandb_proj_name "apr1-rfm-grokking" \
@@ -29,7 +29,7 @@ do
     --kernel_type "gaussian" \
     --iters 500 \
     --ridge 1e-3 \
-    --bandwidth 1.0 \
+    --bandwidth 0.5 \
     --jac_reg_weight 0.0 \
     --agip_rdx_weight 0.0 \
     --agop_sma_size 2 \
